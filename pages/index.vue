39c083ab384a2ebe741e054f9670be17fa3d909b
mohamed799 <ibrahi16@purdue.edu>
2020-12-15 05:02:16 -0500

    My new website, yay!

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+<!--    This is a comment used CTRL+/ -->
+    <h1 class="WebHeader">
+      {{ name }}!
+    </h1>
+    <button @click="setname">
+      Change Name
+    </button>
+  </div>
+</template>
+<style>
+/*This is a comment -- Used CTRL + / */
+.WebHeader {
+  color: #ff0000
+}
+</style>
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      name: "Vikram"
+    }
+  },
+  methods: {
+    setname() {
+      this.name = "Matthew" + "Vikram" + "Mohamed"
+    }
+  }
+
+}
+</script>
